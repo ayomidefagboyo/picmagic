7ff048964f3042991ceaef55d1654d57d592d0c7
ayomide <Ayomide_fagboyo@yahoo.com>
2021-05-20 23:32:33 +0100

    update main.go

--- picmagic/main.go
+++ picmagic/main.go
@@ -14,6 +14,9 @@ func check(err error) {
 func main() {
 	if len(os.Args) < 3 {
 		log.Fatalln("image path is required, i could have guessed it right though ;)")
-
 	}
+	imgPath := os.Args[1]
+	f , err := os.Open(imgPath) check(err) defer f.Close()
+	size := img.Bounds().size()
+	rect := image.Rect(0,0,Size.X, size.Y)
 }
